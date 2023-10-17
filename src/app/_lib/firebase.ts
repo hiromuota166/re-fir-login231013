ecabf9c99db3426f8d5e83e2712aa3eef4e33091
hiromuota166 <otahiromu166@gmail.com>
2023-10-18 00:14:01 +0900

    ログイン動作確認完了

--- src/app/_lib/firebase.ts
+++ src/app/_lib/firebase.ts
@@ -1,9 +1,9 @@
-import { FirebaseApp, getApps, initializeApp } from "firebase/app";
-import { Auth, getAuth } from "firebase/auth";
-import { Firestore, getFirestore } from "firebase/firestore";
+import { getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  apiKey: process.env.NEXT_PUBLIC_API,
   authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECTID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
@@ -12,18 +12,9 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
 };
 
-let firebaseApp: FirebaseApp;
-let auth: Auth;
-let firestore: Firestore;
-
-// サーバーサイドでレンダリングするときにエラーが起きないようにするための記述
-//undefinedの時に初期化がを行われないようにする
-if (typeof window !== "undefined" && !getApps().length) {
-  firebaseApp = initializeApp(firebaseConfig);
-  auth = getAuth();
-  firestore = getFirestore();
-  console.log("initialized firebase app");
+if (!getApps()?.length) {
+  initializeApp(firebaseConfig);
 }
 
+export const auth = getAuth();
 export const db = getFirestore();
-export { firebaseApp, auth };
